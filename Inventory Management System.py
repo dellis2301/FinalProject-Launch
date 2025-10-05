a45a6c640cb9342a175611bf4bd0710eb272c76a
dellis2301 <dellis75@ivytech.edu>
2025-10-05 13:27:10 -0400

    Created Inventory Management System.py

---
+++ Inventory Management System.py
@@ -0,0 +1,11 @@
+"""
+Inventory Management System - Meijer
+Author: Team 4
+Description: Our team proposes to build a Python-based inventory management system for Meijer. 
+The system will allow store employees to track products, manage stock levels, and generate reports on inventory and sales.
+"""
+
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import datetime
+from array import array
